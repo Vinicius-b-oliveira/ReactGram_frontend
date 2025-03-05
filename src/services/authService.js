78250f2520057196353a8d642b25734c74a65fef
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-03-05 12:28:23 -0300

    Criando o service de autenticação

---
+++ src/services/authService.js
@@ -0,0 +1,23 @@
+import { api, requestConfig } from "../utils/config";
+
+const register = async (data) => {
+    const config = requestConfig("POST", data);
+
+    try {
+        const res = await fetch(`${api}/users/register`, config);
+
+        const json = await res.json();
+
+        if (json && json.id) {
+            sessionStorage.setItem("user", JSON.stringify(json));
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const authService = {
+    register,
+};
+
+export default authService;
