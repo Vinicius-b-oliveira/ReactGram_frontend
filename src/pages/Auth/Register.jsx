b5623e7bd423fb3571ed2eb096c83bcd2ca8c9bd
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-03-05 10:57:31 -0300

    Implementando controlled inputs no formulario de registro

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -7,11 +7,25 @@ import { Link } from "react-router-dom";
 // Hooks
 import { useState, useEffect } from "react";
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-};
-
 const Register = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const user = {
+            name,
+            email,
+            password,
+            confirmPassword,
+        };
+
+        console.log(user);
+    };
+
     return (
         <div id="register">
             <h2>ReactGram</h2>
@@ -19,10 +33,30 @@ const Register = () => {
                 Cadastre-se para ver as fotos dos seus amigos.
             </p>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Nome" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Senha" />
-                <input type="password" placeholder="Confirme a senha" />
+                <input
+                    type="text"
+                    placeholder="Nome"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Confirme a senha"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
                 <input type="submit" value="Cadastrar" />
             </form>
 
