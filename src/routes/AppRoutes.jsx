e5a8ccbda73e6f63d1b2d31dd64db357b75eb2fc
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-03-06 15:39:15 -0300

    Criando a estrutura da página de edição de perfil

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -9,32 +9,41 @@ import PublicRoute from "./PublicRoute";
 import Home from "../pages/Home";
 import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
+import EditProfile from "../pages/EditProfile";
 
 export const AppRoutes = ({ auth }) => (
     <Routes>
-            <Route
-                path="/"
-                element={
-                    <ProtectedRoute auth={auth}>
-                        <Home />
-                    </ProtectedRoute>
-                }
-            />
-            <Route
-                path="/login"
-                element={
-                    <PublicRoute auth={auth}>
-                        <Login />
-                    </PublicRoute>
-                }
-            />
-            <Route
-                path="/register"
-                element={
-                    <PublicRoute auth={auth}>
-                        <Register />
-                    </PublicRoute>
-                }
-            />
-        </Routes>
+        <Route
+            path="/"
+            element={
+                <ProtectedRoute auth={auth}>
+                    <Home />
+                </ProtectedRoute>
+            }
+        />
+        <Route
+            path="/profile"
+            element={
+                <ProtectedRoute auth={auth}>
+                    <EditProfile />
+                </ProtectedRoute>
+            }
+        />
+        <Route
+            path="/login"
+            element={
+                <PublicRoute auth={auth}>
+                    <Login />
+                </PublicRoute>
+            }
+        />
+        <Route
+            path="/register"
+            element={
+                <PublicRoute auth={auth}>
+                    <Register />
+                </PublicRoute>
+            }
+        />
+    </Routes>
 );
