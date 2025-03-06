f9ffd246997a01358579f000857a912bb9774de7
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-03-06 11:59:59 -0300

    Criando service de login

--- src/services/authService.js
+++ src/services/authService.js
@@ -22,9 +22,28 @@ const logout = () => {
     sessionStorage.removeItem("user");
 };
 
+const login = async (data) => {
+    const config = requestConfig("POST", data);
+
+    try {
+        const res = await fetch(`${api}/users/login`, config);
+
+        const json = await res.json();
+
+        if (json._id) {
+            sessionStorage.setItem("user", JSON.stringify(json));
+        }
+
+        return json;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 const authService = {
     register,
     logout,
+    login,
 };
 
 export default authService;
