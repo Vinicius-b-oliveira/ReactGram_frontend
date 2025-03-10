360a5199ade0c7b591c246d566928d51478c88a7
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-03-10 15:33:08 -0300

    Estados do usuário e estilos iniciais

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -14,11 +14,39 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 // redux
+import { getUserDetails } from "../../slices/userSlice";
 
 const Profile = () => {
+    const { id } = useParams();
+
+    const dispatch = useDispatch();
+
+    const { user, loading } = useSelector((state) => state.user);
+
+    const { user: userAuth } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        dispatch(getUserDetails(id));
+    }, [dispatch, id]);
+
+    if (loading) {
+        return <p>Carregando...</p>;
+    }
+
     return (
-        <div>
-            <h1>Profile</h1>
+        <div id="profile">
+            <div className="profile_header">
+                {user.profileImage && (
+                    <img
+                        src={`${uploads}/users/${user.profileImage}`}
+                        alt="Imagem de perfil"
+                    />
+                )}
+                <div className="profile_description">
+                    <h2>{user.name}</h2>
+                    <p>{user.bio}</p>
+                </div>
+            </div>
         </div>
     );
 };
