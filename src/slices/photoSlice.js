68641ced238ce9fe927d14b37cc44e274b729a13
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-03-11 14:38:49 -0300

    Criando slice para pegar todas as fotos do usuário

--- src/slices/photoSlice.js
+++ src/slices/photoSlice.js
@@ -25,6 +25,17 @@ export const publishPhoto = createAsyncThunk(
     }
 );
 
+export const getUserPhotos = createAsyncThunk(
+    "photos/userPhotos",
+    async (id, ThunkAPI) => {
+        const token = ThunkAPI.getState().auth.user.token;
+
+        const data = await photoService.getUserPhotos(id, token);
+
+        return data;
+    }
+);
+
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -51,6 +62,16 @@ export const photoSlice = createSlice({
                 state.loading = false;
                 state.error = action.payload;
                 state.photo = {};
+            })
+            .addCase(getUserPhotos.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(getUserPhotos.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.photos = action.payload;
             });
     },
 });
