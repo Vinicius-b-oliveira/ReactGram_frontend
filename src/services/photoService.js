f04ac2276910befb6c88a75cb6e67224d3ec70e5
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-03-13 14:36:22 -0300

    Criando service de atualização de fotos

--- src/services/photoService.js
+++ src/services/photoService.js
@@ -42,6 +42,20 @@ const deletePhoto = async (id, token) => {
     }
 };
 
-const photoService = { publishPhoto, getUserPhotos, deletePhoto };
+const updatePhoto = async (data, id, token) => {
+    const config = requestConfig("PUT", data, token);
+
+    try {
+        const res = await fetch(`${api}/photos/${id}`);
+
+        const json = await res.json();
+
+        return json;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const photoService = { publishPhoto, getUserPhotos, deletePhoto, updatePhoto };
 
 export default photoService;
