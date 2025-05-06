f51982547530c88367da794f70bd91b10f506a0d
Vini <viniciusbuenodeoliveira2017@gmail.com>
2025-05-06 13:56:46 -0300

    Criando hook de busca

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -19,10 +19,13 @@ import { useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "../../hooks/useQuery";
 
 const Navbar = () => {
     const { auth } = useAuth();
     const { user } = useSelector((state) => state.auth);
+
+    const [query, setQuery] = useState("");
 
     const navigate = useNavigate();
 
@@ -35,14 +38,27 @@ const Navbar = () => {
         navigate("/login");
     };
 
+    const handleSearch = (e) => {
+        e.preventDefault();
+
+        if (query) {
+            return navigate(`/search?q=${query}`);
+        }
+    };
+
     return (
         <header className="page_header">
             <Link className="brand" to="/">
                 ReactGram
             </Link>
-            <form className="search_form">
+            <form className="search_form" onSubmit={handleSearch}>
                 <BsSearch />
-                <input type="text" placeholder="Pesquisar" />
+                <input
+                    type="text"
+                    placeholder="Pesquisar"
+                    onChange={(e) => setQuery(e.target.value)}
+                    value={query}
+                />
             </form>
             <nav>
                 <ul className="nav_links">
