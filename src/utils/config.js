aa846d0ee619f8c2fedce6c0b60fbb694ce833bc
Vini <viniciusbuenodeoliveira2017@gmail.com>
2025-05-06 17:32:10 -0300

    Url do backend

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const api = import.meta.env.VITE_API_URL;
+export const api = "https://reactgram-api-tvsx.onrender.com/api";
 
 export const requestConfig = (method, data, token = null, image = null) => {
     let config;
